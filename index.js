6c3518dad52d4d746d57fc8338ed0d91a00bb5f9
hugokody <kody.hugo@gmail.com>
2024-11-20 02:35:50 -0300

    segundo commit com codigo finalizado

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("desafio nivel de heroi");
+let nomeDoHeroi = "hugo";
+let xp = 7500;
+let nivel = " ";
+
+switch (true) {
+    case xp > 0 && xp <= 1000:
+       nivel = "Ferro";
+       break;
+
+    case xp > 1001 && xp <= 2000:
+        nivel = "Bronze";
+        break;
+
+    case xp > 2001 && xp <= 5000:
+        nivel = "Prata";
+        break;
+
+    case xp > 5001 && xp <= 7000:
+        nivel = "Ouro";
+        break;
+
+    case xp > 7001 && xp <= 8000:
+        nivel = "Platina";
+        break;
+
+    case xp > 8001 && xp <= 9000:
+        nivel = "Ascendente";
+        break;
+
+    case xp > 9001 && xp <= 10000:
+        nivel = "Imortal";
+        break;
+
+    case xp > 10001:
+        nivel = "Radiante";
+        break;
+
+    default:
+        nivel = "diferente";
+        break;
+}
+
+
+
+console.log("O Herói de nome: ** " + nomeDoHeroi + " ** está no nível de : ** " + nivel + " ** .");
